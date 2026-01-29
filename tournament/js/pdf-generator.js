18b565f43dbddb8f9824275d3ac06d3b9600c7bd
Jeremy Penderman <jeremypenderman@Mac.lan>
2026-01-28 22:56:47 -0500

    Adjust indicator spacing slightly more right

--- tournament/js/pdf-generator.js
+++ tournament/js/pdf-generator.js
@@ -516,7 +516,7 @@ function drawYardageCard(doc, tournament, registration, dayConditions, x, y, day
                 doc.setTextColor(...adjColor);
                 // Position as superscript: right of yardage, raised up
                 const yardageWidth = doc.getTextWidth(`${dist}`);
-                doc.text(adjText, colX + yardageWidth + 8, currentY + 6);
+                doc.text(adjText, colX + yardageWidth + 11, currentY + 6);
             }
         };
 
